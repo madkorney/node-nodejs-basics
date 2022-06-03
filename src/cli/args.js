f45d83a33254ec1d15bd4fe84637f29e97002006
korney <madkorney@gmail.com>
2022-06-04 01:46:24 +0300

    feat: implement args subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let propName =  process.argv[3];
+    let prop2Name = process.argv[5];
+    console.log(`propName is ${propName}, prop2Name is ${prop2Name}`);
 };
+
+// implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it)
+// and prints them to the console in the format propName is value, prop2Name is value2
