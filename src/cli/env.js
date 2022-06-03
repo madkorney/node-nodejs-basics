795a9ed14ab17f109df434a8dc62f9b731a3499d
korney <madkorney@gmail.com>
2022-06-04 01:46:54 +0300

    feat: implement cli/env subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
 export const parseEnv = () => {
-    // Write your code here 
+  let parsedVars = [];
+
+  for (let key in process.env) {
+    if (key.substring(0,4) === 'RSS_') {
+      console.log(`${key} = ${process.env[key]};`);
+      parsedVars.push(process.env[key]);
+    }
+  }
+  if (parsedVars.length === 0) {
+    console.log(`No 'RSS_' variables found..`);
+    return false
+  }
+  return parsedVars;
 };
+
+
