c040c62adb2e69e32df22392d8262871297e6d6a
korney <madkorney@gmail.com>
2022-06-05 20:25:03 +0300

    feat: implement streams subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import { pipeline } from 'stream';
+
 export const read = async () => {
-    // Write your code here 
+
+  const fileName = '/files/fileToRead.txt';
+  const filePathName = path.join(__dirname, fileName);
+  try {
+    const fd = await fs.open(filePathName);
+    const readStream = fd.createReadStream();
+    pipeline(
+      readStream,
+      process.stdout,
+      error => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+        }
+      }
+    );
+  }
+  catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 };
