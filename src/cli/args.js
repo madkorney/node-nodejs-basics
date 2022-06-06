0f470028e852ccd416387ca98b6db1665ef92099
Sergey Kornienko <madkorney@gmail.com>
2022-06-06 21:28:20 +0300

    fix: fix solution to handle variable input

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,19 @@
 export const parseArgs = () => {
-    let propName =  process.argv[3];
-    let prop2Name = process.argv[5];
-    console.log(`propName is ${propName}, prop2Name is ${prop2Name}`);
+
+  let args =  process.argv.slice(2);
+  let userArgs = [];
+
+  args.forEach( (item, index, arr) => {
+    if (item.startsWith('--')) {
+      const argStr = `${item} is ${arr[index + 1]}`;
+      userArgs.push(argStr);
+    }
+  });
+
+  console.log(userArgs.join(','));
 };
 
 // implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it)
 // and prints them to the console in the format propName is value, prop2Name is value2
+
+parseArgs();
